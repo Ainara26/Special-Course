789783f6c7715b777eba22f56bfe81677adce864
Ainara26 <143824186+Ainara26@users.noreply.github.com>
2025-02-05 15:22:24 +0100

    example

--- mediaOpt_example.py
+++ mediaOpt_example.py
@@ -12,3 +12,114 @@ from gpytorch.mlls import ExactMarginalLogLikelihood
 from tqdm import tqdm
 
 torch.set_default_dtype(torch.double)
+
+# media components
+MEDIA = ["FeSO4", "K2HPO4", "NH4Cl", "CaCl2", "NaCl", "Na2SO4"]
+
+# boundaries
+BOUNDS = [(0.0, 1.0)] * len(MEDIA) #minimum (0) and maximum (1) boundaries for each media component (len(MEDIA))
+    #BOUNDS = [
+    #[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # Minimum values (lower bound)
+    #[1.0, 1.0, 1.0, 1.0, 1.0, 1.0]   # Maximum values (upper bound)
+    #]
+
+# shapes
+Q = 12 #number of batch of experiments that run in parallel
+D = len(MEDIA) #dimensionality of the optimisation problem (in this case, the 6 media components)
+
+#Hartmann function to have reference values for testing
+TRUTH = Hartmann(dim=D, bounds=BOUNDS, negate=True, noise_std=0.05)
+TRUE_ARGOPTIMUM = (0.20169, 0.150011, 0.476874, 0.275332, 0.311652, 0.6573) #best possible combination of media components
+TRUE_OPTIMUM = 3.32237 #best results of growth
+
+# Fix random seed to control the randomness of the function
+SEED = 12345
+torch.manual_seed(seed=SEED)
+
+#We created a function called get_next_batch_of_designs. 
+    #The goal of this function is to o select the next set of experimental conditions (designs) that optimize log Expected Improvement (pLogEI).
+    #This function generates a batch of new experiments (q points) based on previously tested conditions (x,y).
+def get_next_batch_of_designs(
+    x: torch.Tensor, #past experimental conditions
+    y: torch.Tensor, #observed KPI values, for example growth rate
+    bounds: torch.Tensor | list[tuple[float, float]], #min and max value of each variable
+    q: int, #number of experiments to generate
+    seed: int | None = None, #random seed for reproducibility
+) -> tuple[torch.Tensor, float]: #returns (batch of designs, log-scale acquisition value)
+    
+    #handling random seed
+    if seed is not None:
+        torch.manual_seed(seed=seed)
+    
+    #transposes it to match the format required by BoTorch (d x 2)-> each row will have the min and max of each component respectively
+    bounds_t = torch.Tensor(bounds).T
+
+    #creation of a GP model that can predict KPI values for untested conditions, along with uncertainty estimates
+    #surrogate model
+    gp_model = SingleTaskGP(
+        train_X=x,
+        train_Y=y,
+        input_transform=Normalize(d=x.shape[1]),
+        outcome_transform=Standardize(m=1),
+    )
+
+    #Trains the GP model to accurately approximate the objective function.
+    mll = ExactMarginalLogLikelihood(gp_model.likelihood, gp_model)
+    fit_gpytorch_mll(mll)
+
+    #acquisition function
+    sampler = SobolQMCNormalSampler(torch.Size([q]), seed=seed)
+    qlei = qLogExpectedImprovement(
+        model=gp_model,
+        best_f=y.max(),
+        sampler=sampler,
+    )
+
+    #optimizing the acquisition function
+    candidates, joint_acq = optimize_acqf(
+        acq_function=qlei,
+        bounds=bounds_t,
+        q=q,
+        num_restarts=2,
+        raw_samples=10,
+        sequential=False,
+    )
+
+    #return new experimental conditions
+    return candidates, float(joint_acq)
+
+def main():
+    bounds_tensor = torch.Tensor(BOUNDS).T
+    first_x = draw_sobol_samples(
+        bounds=bounds_tensor,
+        q=Q,
+        n=1,
+        seed=SEED,
+    ).squeeze(0)
+    rounds = 2
+    x = first_x
+    y = torch.Tensor([])
+    best = []
+    pbar = tqdm(range(rounds))
+    next_x = first_x
+    for r in pbar:
+        next_y = TRUTH.forward(next_x, noise=True).unsqueeze(-1)
+        y = torch.concat([y, next_y])
+        print(y.shape)
+        next_x, _ = get_next_batch_of_designs(
+            x=x,
+            y=y,
+            bounds=BOUNDS,
+            q=Q,
+            seed=SEED,
+        )
+        best_so_far = float(y.max())
+        msg = f"Best after round {r}: " + str(best_so_far)
+        pbar.set_description(msg)
+        best.append([r, best_so_far])
+        x = torch.concat([x, next_x])
+    print(x)
+
+
+if __name__ == "__main__":
+    main()
