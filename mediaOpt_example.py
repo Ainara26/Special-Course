da2e090ca1f1747f1afd91d81f330b45cef43d53
Ainara26 <143824186+Ainara26@users.noreply.github.com>
2025-02-05 14:24:00 +0100

    initial run

--- mediaOpt_example.py
+++ mediaOpt_example.py
@@ -1,9 +1,14 @@
-import pandas as pd
 import torch
+from botorch.acquisition import qLogExpectedImprovement
+from botorch.fit import fit_gpytorch_mll
+from botorch.models import SingleTaskGP
+from botorch.models.transforms.input import Normalize
+from botorch.models.transforms.outcome import Standardize
+from botorch.optim import optimize_acqf
+from botorch.sampling import SobolQMCNormalSampler
 from botorch.test_functions.synthetic import Hartmann
 from botorch.utils.sampling import draw_sobol_samples
-from matplotlib import pyplot as plt
+from gpytorch.mlls import ExactMarginalLogLikelihood
 from tqdm import tqdm
-import numpy as np
 
-from src.mediabo.optimisation import get_next_batch_of_designs
+torch.set_default_dtype(torch.double)
